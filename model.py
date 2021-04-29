fcc931b4d96da98d554403b878a3ccd1799ef8e4
fahim <fahim.cross@gmail.com>
2021-04-29 12:09:02 +0600

    Updated model.py
    Renamed ValueNetwork and PolicyNetwork to CriticNetwork and ActorNetwork. Added Batch Normalization layer in CriticNetwork for normalizing actions.

--- model.py
+++ model.py
@@ -1,45 +1,85 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class ValueNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions):
-        super(ValueNetwork, self).__init__()
-
-        self.fc = nn.Sequential(
-            nn.Linear(num_inputs + num_actions, 1024),
-            nn.LeakyReLU(),
-            nn.Linear(1024, 128),
-            nn.LeakyReLU(),
-            nn.Linear(128, 1)
-        )
-
-    def forward(self, state, action):
-        x = torch.cat([state, action], 1)
-        return self.fc(x)
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return -lim, lim
 
 
-class PolicyNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions):
-        super(PolicyNetwork, self).__init__()
-        self.num_actions = num_actions
+class ActorNetwork(nn.Module):
+    """Actor (Policy) Model."""
 
-        self.fc = nn.Sequential(
-            nn.Linear(num_inputs, 512),
-            nn.LeakyReLU(),
-            nn.Linear(512, 128),
-            nn.LeakyReLU(),
-            nn.Linear(128, num_actions),
-            nn.Tanh()
-        )
+    def __init__(self, state_size, action_size, seed=10, fc_units=384):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(ActorNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc_units)
+        self.fc2 = nn.Linear(fc_units, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
-        return self.fc(state)
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.relu(self.fc1(state))
+        return torch.tanh(self.fc2(x))
 
     def get_action(self, state):
         state = torch.FloatTensor(state).unsqueeze(0).to(device)
         action = self.forward(state)
-        return action.detach().cpu().numpy()[0, 0:self.num_actions]
+        return action.squeeze(0).detach().cpu().numpy()
+
+
+class CriticNetwork(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed=10, fcs1_units=512, fc2_units=256, fc3_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
+        super(CriticNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.bn = nn.BatchNorm1d(action_size)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc3_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
+        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.leaky_relu(self.fcs1(state))
+        action = self.bn(action)
+        x = torch.cat((xs, action), dim=1)
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        return self.fc4(x)
 
